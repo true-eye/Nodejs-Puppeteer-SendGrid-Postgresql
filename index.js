3512d0156029ae382b8d89c9369bd2ff10939853
AngularMaster <trueeyedev@gmail.com>
2019-05-07 09:14:52 -0400

    saintfred

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var path = require('path')
 
 var manageDBFile = require('./manageDBFile/index.js')
 var module_onenessboutique = require('./onenessboutique.js')
-var module_finishline_men = require('./finishline_men.js')
 var module_citygear = require('./citygear.js')
 var module_jimmyjazz = require('./jimmyjazz.js')
 
@@ -15,7 +14,6 @@ var module_jimmyjazz_women = require('./jimmyjazz_women')
 var module_jimmyjazz_grade = require('./jimmyjazz_grade')
 
 var module_kickz = require('./kickz.js')
-var module_saintalfred = require('./saintalfred')
 var module_shelta = require('./shelta')
 var module_sneakerpolitics = require('./sneakerpolitics')
 var module_overkillshop = require('./overkillshop')
@@ -52,8 +50,9 @@ var module_undefeat_nike = require('./undefeat_nike')
 var module_undefeat_jordan = require('./undefeat_jordan')
 var module_sneakersnstuff = require('./sneakersnstuff')
 var module_saintalfred = require('./saintalfred')
+var module_finishline_men = require('./finishline_men')
 
-var developer_mode = true
+var developer_mode = false
 
 app = express()
 
@@ -223,9 +222,9 @@ let allWebsites = async () => {
             </thead>
             <tbody>`
 
-  //message += await module_finishline_men.scrap_finishline_men("scrap_finishline_men");
+  //message += await scrap('finishline_men', module_finishline_men.default)
+
   message += await scrap('saintalfred', module_saintalfred.default)
-  /*
   message += await scrap('kicksusa_men', module_kicksusa_men.default)
   message += await scrap('kicksusa_women', module_kicksusa_women.default)
   message += await scrap('kicksusa_kids', module_kicksusa_kids.default)
@@ -264,7 +263,7 @@ let allWebsites = async () => {
   message += await scrap('undefeat_nike', module_undefeat_nike.default) //complete
   message += await scrap('undefeat_jordan', module_undefeat_jordan.default) //complete
   message += await scrap('sneakersnstuff', module_sneakersnstuff.default) //complete
-*/
+
   //message += await module_overkillshop.scrap_overkillshop("scrap_overkillshop");
   message += `</tbody></table></body></html>`
   /*
